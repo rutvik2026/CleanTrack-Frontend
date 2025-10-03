e05563313aa9cfc87a8e58dfc90afec2ee0c0954
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-10-04 00:19:23 +0530

    Update NewToilet.jsx

--- src/NewToilet.jsx
+++ src/NewToilet.jsx
@@ -30,7 +30,7 @@ const NewToilet = () => {
     try {
       const res = await fetch("/api/newtoilet", {
         method: "POST",
-        headers: { "Content-Type": `${import.meta.env.Backend_URL}/application/json` },
+        headers: { "Content-Type": `${import.meta.env.VITE_BACKEND_URL}/application/json` },
         body: JSON.stringify(formData),
       });
 
